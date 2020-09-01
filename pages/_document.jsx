3f91d38bd3e1f8b0fa7878593d51e49b1ee77ff6
Victor Garcia <victorgrubiodl@gmail.com>
2020-09-01 21:05:41 +0200

    Final test, lets wait for tomorrow results

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,17 +2,13 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import ReactGA from 'react-ga';
 
 var trackingId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID; // Replace with your Google Analytics tracking ID
-trackingId = 'UA-176429750-1'
-function initializeReactGA() {
-    ReactGA.initialize(trackingId);
-    ReactGA.pageview('/');
-}
-
+trackingId = 'UA-176429750-1';
 
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx){
-        initializeReactGA();
+        ReactGA.initialize('UA-176429750-1');
+        ReactGA.pageview(window.location.pathname);
         const initialProps = await Document.getInitialProps(ctx);
         let props = {...initialProps };
         return props;
@@ -21,13 +17,6 @@ export default class MyDocument extends Document {
         return(
             <html lang="en">
                 <Head>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176429750-1"></script>
-                    <script>
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments)}
-                        gtag('js', new Date());
-                        gtag('config', 'UA-176429750-1');
-                    </script>
                     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
                     <link rel='shortcut icon' type='image/x-icon' href='/static/assets/template/ronaldo/favicon.ico' />
                     <link rel="stylesheet" href="/static/assets/template/ronaldo/css/production.min.css"/>

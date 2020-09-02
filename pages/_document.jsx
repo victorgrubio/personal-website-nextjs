ba081de92a917886ba1ab6a45fc789958be92e01
Victor Garcia <victorgrubiodl@gmail.com>
2020-09-02 10:22:53 +0200

    Trying to make ga work

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,8 +7,6 @@ trackingId = 'UA-176429750-1';
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx){
-        ReactGA.initialize('UA-176429750-1');
-        if (typeof window !== 'undefined') ReactGA.pageview(window.location.pathname);
         const initialProps = await Document.getInitialProps(ctx);
         let props = {...initialProps };
         return props;
@@ -17,6 +15,17 @@ export default class MyDocument extends Document {
         return(
             <html lang="en">
                 <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176429750-1" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'UA-176429750-1');
+                            `,
+                        }}
+                    />
                     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
                     <link rel='shortcut icon' type='image/x-icon' href='/static/assets/template/ronaldo/favicon.ico' />
                     <link rel="stylesheet" href="/static/assets/template/ronaldo/css/production.min.css"/>
